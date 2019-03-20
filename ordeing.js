6de27fb886a4f4b531f3c5d9cad0f28e575c09b5
Malachi Foster <MAntraasura0>
2019-03-19 20:33:45 -0400

    hw

--- ordeing.js
+++ ordeing.js
@@ -7,9 +7,56 @@ let array = [7, 3, 17, 6, 5, 0]
       array[i] = array[i + 1]
       array[i + 1] = temp
 
+
     }
 
   }
+
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+
+
+      }
+
+    }
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+
+
+      }
+
+    }
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+
+
+      }
+
+    }
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+
+
+      }
+
+    }
+
 
 
 console.log(array)
